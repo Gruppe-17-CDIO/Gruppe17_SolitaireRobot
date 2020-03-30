c7706296c660064882069c68c7293bf709c15020
Anders Frandsen <anfran@anfran.dk>
2020-03-30 20:47:44 +0200

    Algorithm check piles, unittest

--- Logic/src/main/java/Logic.java
+++ Logic/src/main/java/Logic.java
@@ -1,10 +1,58 @@
+import dataObjects.Card;
 import dataObjects.Move;
 import dataObjects.SolitaireState;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Logic implements I_Logic {
     @Override
-    public List<Move> getMoves(SolitaireState cards) {
-        return null;
+    public List<Move> getMoves(SolitaireState state) {
+        List<Move> moves = new ArrayList<>();
+        /*
+         * Outer loops iterates from pile 1 to 7 and every card in each pile.
+         */
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < state.getPiles().get(i).size(); j++) {
+                Card card = state.getPiles().get(i).get(j);
+
+                if (card.getStatus() == Card.Status.FACEDOWN) {
+                    // turn up
+                    continue;
+                }
+
+                // Inner loop: piles
+                for (int p = 0; p < 7; p++) {
+                    if (p == i)
+                        continue;
+
+                    Card destCard = state.getPiles().get(p).get(state.getPiles().get(p).size() - 1);
+                    if (destCard.getStatus() == Card.Status.FACEDOWN)
+                        continue;
+
+                    int[] position = {i, j};
+
+                    if (card.getRank() == destCard.getRank() - 1 && card.getColor() != destCard.getColor()) {
+                        moves.add(
+                                new Move(Move.MoveType.MOVE, position, Move.DestinationType.PILE, p)
+                        );
+                    }
+                }
+
+                // Draw card
+                Card draw = state.getDrawnCards().get(0);
+                int[] position = {};
+
+                if (draw.getRank() == card.getRank() - 1 && draw.getColor() != card.getColor()) {
+                    moves.add(
+                            new Move(Move.MoveType.DRAW, position, Move.DestinationType.PILE, i)
+                    );
+                }
+            }
+        }
+
+        if (moves.isEmpty())
+            return null;
+        else
+            return moves;
     }
 }
--- Logic/src/test/java/LogicTest.java
+++ Logic/src/test/java/LogicTest.java
@@ -1,17 +1,21 @@
 import dataObjects.Move;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import dataObjects.SolitaireState;
 import CV_simulation.StateGenerator;
-import sun.jvm.hotspot.utilities.Assert;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LogicTest {
     @Test
     void getMovesTest() {
-        //Arrange
+
+        // Arrange
         SolitaireState state = null;
         I_Logic logic = new Logic();
-        Move move = null;
+        List<Move> moves;
 
         try {
             state = StateGenerator.getState(1);
@@ -20,18 +24,30 @@ class LogicTest {
             System.out.println(ex.toString());
         }
 
-        //Act
-        move = logic.getMove(state);
+        // Act
+        moves = logic.getMoves(state);
 
-        //Assert
-        /*
-         * Possible moves - ordered, but not weighted:
-         * piles[2][0] -> piles[1]
-         * piles[5][0] -> piles[6]
-         * piles[3][3] -> piles[0]
-         * Drawn[0] -> Foundation[1]
-         * Drawn[1] -> piles[3]
-         */
-        assertEquals(move.getAvailableType(), Move.Type.DRAW);
+        // Assert: Possible moves - ordered by algorithm, but not weighted yet:
+
+        // piles[2,2] -> piles[1]
+        int[] pos = {2,2};
+        assertEquals(Move.MoveType.MOVE, moves.get(0).getMoveType());
+        assertArrayEquals(pos, moves.get(0).getPosition());
+        assertEquals(Move.DestinationType.PILE, moves.get(0).getDestinationType());
+        assertEquals(1, moves.get(0).getDestPosition());
+
+        // piles[3,3] -> piles[0]
+        pos[0] = 3; pos[1] = 3;
+        assertEquals(Move.MoveType.MOVE, moves.get(1).getMoveType());
+        assertArrayEquals(pos, moves.get(1).getPosition());
+        assertEquals(Move.DestinationType.PILE, moves.get(1).getDestinationType());
+        assertEquals(0, moves.get(1).getDestPosition());
+
+        // piles[5,5] -> piles[6]
+        pos[0] = 5; pos[1] = 5;
+        assertEquals(Move.MoveType.MOVE, moves.get(2).getMoveType());
+        assertArrayEquals(pos, moves.get(2).getPosition());
+        assertEquals(Move.DestinationType.PILE, moves.get(2).getDestinationType());
+        assertEquals(6, moves.get(2).getDestPosition());
     }
 }
