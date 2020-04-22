7d630995d9b6c60e2e9ba00770139f9c90fd70c4
ErlendTyrmi <erlend@tyrmi.com>
2020-04-22 11:07:02 +0200

    drawn cards is only one card!

--- ComputerVision/src/main/java/I_ComputerVisionController.java
+++ ComputerVision/src/main/java/I_ComputerVisionController.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface I_ComputerVisionController {
     // Return all fully visible cards as a list to the controller.
     // Empty piles should be in the list, so the length is always 12:
-    // drawn card, foundation 1-4, pile 1-7
+    // 0 = drawn card, foundation 1-4, pile 5-13
     List<Card> getSolitaireCards(Image img);
 }
