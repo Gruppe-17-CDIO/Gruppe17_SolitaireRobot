f73ab1ca735ee0be0c6b5bd77bee662193a8f7d4
ErlendTyrmi <erlend@tyrmi.com>
2020-04-30 01:09:09 +0200

    Commants changes

--- Control/src/main/java/Controller.java
+++ Control/src/main/java/Controller.java
@@ -21,15 +21,14 @@ public class Controller implements I_Controller {
             Card[] cardData = CV_Controller.getSolitaireCards(img);
             // 2. reset history and make new logfile
             // 3. Calculate State
-            // 4. Add to history
             SolitaireState state = stateManager.initiate(cardData);
-            // 5. Get moves
+            // 4. Get moves
             List<Move> moves = logic.getMoves(state);
-            // 6. Store state and moves
+            // 5. Store state and moves
             stateManager.saveState(state, moves);
-            // 6. Store the previous move. (Not list)
+            // 6. Keep the previous move. (Not list)
             prevMove = moves.get(0);
-            // 6. Return move and history
+            // 7. Return move and history
             callback.OnSuccess(moves, stateManager.getHistory());
         } catch (Exception e) {
             callback.OnError(e);
@@ -49,7 +48,7 @@ public class Controller implements I_Controller {
             List<Move> moves = logic.getMoves(state);
             // 5. Save state and moves
             stateManager.saveState(state, moves);
-            // 6. Save this move as previous
+            // 6. Keep the previous move. (Not list)
             prevMove = moves.get(0);
             // 7. Return move and history
             callback.OnSuccess(moves, stateManager.getHistory());
