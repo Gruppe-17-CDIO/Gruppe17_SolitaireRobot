9d8c5bc867268132eca933df8a060c75d028a8dc
ErlendTyrmi <erlend@tyrmi.com>
2020-06-04 14:06:03 +0200

    TopCards object instead of array

--- ComputerVision/src/main/java/TopCards.md
+++ ComputerVision/src/main/java/TopCards.md
@@ -2,17 +2,24 @@
 
 ##Usage
 
-TopCards are the top 'fully visible' cards that computer vision registers, and returns to logic.
+TopCards is a data object for the top 'fully visible' cards that computer vision registers. The data strucure is returned to Control and handled there.
 
 Set the drawn card with:
+
 ```setDrawnCard(Card)```
 
 Set foundations (max 4) with:
+
 ```setFoundations(List<Card>)```
 
 Set piles (columns, max 7) with:
+
  ```setPiles(List<Card>)```
 
 The two last methods throw an exception if there are too many cards in the list.
 
-Null values are OK, because there may be empty piles / foundations.
+Null values are OK during the game, because there may be empty piles / foundations.
+
+Not to be confused with the final state object, that has lists of cards, this object has only one card per visible pile.
+
+> Blame: Erlend & Rasmus
--- Logic/src/main/java/cardCalculator/CardCalculator.java
+++ Logic/src/main/java/cardCalculator/CardCalculator.java
@@ -3,32 +3,44 @@ package cardCalculator;
 import dataObjects.Card;
 import dataObjects.Move;
 import dataObjects.SolitaireState;
+import dataObjects.TopCards;
 import stateBuilding.StateGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CardCalculator {
+    // This class is responsible for
+    // 1. Making a state based on initial TopCard object from CV.
+    // 2. Updating state based on move and previous state.
+    // 3. Checking state against the new TopCard object from CV.
 
-    public SolitaireState initiateState(Card[] cardData) throws Exception {
+    /**
+     * @param topCards, the cards as seen by computer vision on initial setup
+     * @return state, a Full SoliaireState Object
+     * @throws Exception if CardData cannot be used
+     */
+    public SolitaireState initiateState(TopCards topCards) throws Exception {
         // Check input
-        if (cardData == null) {
-            throw new Exception("Card data was null.");
+        if (topCards == null) {
+            throw new Exception("initiateState(): Card data was null.");
         }
-        if (cardData.length != 12) {
-            throw new Exception("Card data should have 12 slots. Was " + cardData.length);
+        if (topCards.getDrawnCard() == null) {
+            throw new Exception("initiateState(): Missing a drawn card in the setup. Draw exactly one card before calling initiate.");
         }
-        for (int i = 0; i < 12; i++) {
-            if (i == 0 || i > 4) {
-                if (cardData[i] == null) {
-                    throw new Exception("Card Data " + i + " was null. Drawn card (0) and piles (5-11) should be " +
-                            "valid cards when a new game is set up");
-                }
-            }
+        if (topCards.getFoundations() != null) {
+            throw new Exception("initiateState(): Can't start out with foundations already present.");
         }
+        if (topCards.getPiles().size() != 7) {
+            throw new Exception("initiateState(): Excpected seven top cards, one for each pile (column).");
+        }
+
+
         SolitaireState state = StateGenerator.getState(999); // State template
 
-        state.setDrawnCard(cardData[0]);
+        ArrayList<Card> drawnCards = new ArrayList<>();
+        drawnCards.add(topCards.getDrawnCard());
+        state.setDrawnCards(drawnCards);
 
         ArrayList<Card> foundations = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
@@ -38,15 +50,23 @@ public class CardCalculator {
 
         List<List<Card>> piles = state.getPiles();
         for (int i = 0; i < 7; i++) {
-            piles.get(i).add(cardData[i + 5]); // Add the 7 visible cards
+            piles.get(i).add(topCards.getPiles().get(i)); // Add the 7 visible cards
         }
         state.setPiles(piles);
         // Do not save state here
         return state;
     }
 
+    /**
+     * Updates the state based on previous state and previous move.
+     *
+     * @param prevState
+     * @param move
+     * @return state
+     */
     public SolitaireState updateState(SolitaireState prevState, Move move) {
         SolitaireState state = prevState;
+        // TODO: Fucking implement!
         // State = state + move
         // CONSIDER DEEP COPY WHEN UPDATNG STATE
 
@@ -55,7 +75,7 @@ public class CardCalculator {
         if (mvoetype == DRAW) update drawn, update stock
         if (movetype == TURN) // TODO: REMOVE THIS DATA FROM MOVE
         if (movetype == MOVE) maybe focus on this first:
-                //if pile which pile?
+                //if pile which pile? Remember drawn cards is apile too.
                 // which card
                 // move card at position + all card up to this pile.length - 1 to destination.
        */
@@ -66,13 +86,19 @@ public class CardCalculator {
         return state;
     }
 
-    public void checkState(Card[] cardData, SolitaireState state) throws Exception {
+    /**
+     * @param topCards
+     * @param state
+     * @throws Exception
+     */
+    public void checkState(TopCards topCards, SolitaireState state) throws Exception {
 
+        // TODO: Update to the new data object
         List<Card> foundations = state.getFoundations();
         List<List<Card>> piles = state.getPiles();
         Card[] stateData = new Card[12];
 
-        stateData[0] = state.getDrawnCard();
+        stateData[0] = state.getDrawnCards().get(0);
         for (int i = 0; i < 5; i++) {
             stateData[i + 1] = foundations.get(i); // 1 to 5 = foundations
         }
@@ -82,9 +108,9 @@ public class CardCalculator {
         }
 
         for (int i = 0; i < 12; i++) {
-            if (!cardData[i].toString().equals(stateData[i].toString())) { // tostring should match
+            if (!topCards[i].toString().equals(stateData[i].toString())) { // tostring should match
                 throw new Exception("Data from Computer Vision does not match calculated array:\n" +
-                        "Computervision:\n" + cardData + "\n" +
+                        "Computervision:\n" + topCards + "\n" +
                         "State as array:\n" + stateData + "\n" +
                         "Retry with 'getNextMove', or 'undo', or restart with 'getFirstMove'!");
             }
