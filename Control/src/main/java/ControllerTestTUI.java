4b3022ea5474a6d39a9a3a2e950956287deb8f62
ErlendTyrmi <erlend@tyrmi.com>
2020-06-11 18:03:11 +0200

    start branch

--- Control/src/main/java/ControllerTestTUI.java
+++ Control/src/main/java/ControllerTestTUI.java
@@ -5,6 +5,8 @@ import javafx.scene.image.Image;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
@@ -14,6 +16,7 @@ import java.util.Stack;
  */
 public class ControllerTestTUI {
     private Controller controller;
+    int gameType;
 
     public static void main(String[] args) {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -23,7 +26,26 @@ public class ControllerTestTUI {
         new ControllerTestTUI().runGame();
     }
 
+
     public void runGame() {
+        int accept = 0;
+        gameType = 1;
+        while (accept == 0) {
+            System.out.println("Manual: 1, Auto First alt: 2, Random Auto: 3");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                gameType = scanner.nextInt();
+                accept = 1;
+                if (gameType > 3 || gameType < 1) {
+                    System.out.println("Invalid game type.");
+                    accept = 0;
+                }
+            } else {
+                System.out.println("Not an int.");
+            }
+        }
+        accept = 0;
+
         controller = new Controller();
 
         controller.setTestModeOn(true, new CompletionCallBack() {
@@ -76,31 +98,41 @@ public class ControllerTestTUI {
     }
 
     private void playTurn(List<Move> moves, Stack<SolitaireState> history) {
-        /*
-        int accept = 0, choice = 0;
-        while (accept == 0) {
-            System.out.println("Choose your move from the list. Integer, then enter.");
-            Scanner scanner = new Scanner(System.in);
-            if (scanner.hasNextInt()) {
-                choice = scanner.nextInt();
-                accept = 1;
-                if (choice > moves.size() - 1 || choice < 0) {
-                    System.out.println("Invalid move index.");
-                    accept = 0;
+        int choice = 0;
+        if (gameType == 1) {
+            int accept = 0;
+            while (accept == 0) {
+                System.out.println("Choose your move from the list. Integer, then enter.");
+                Scanner scanner = new Scanner(System.in);
+                if (scanner.hasNextInt()) {
+                    choice = scanner.nextInt();
+                    accept = 1;
+                    if (choice > moves.size() - 1 || choice < 0) {
+                        System.out.println("Invalid move index.");
+                        accept = 0;
+                    }
+                } else {
+                    System.out.println("Not an int.");
                 }
-            } else {
-                System.out.println("Not an int.");
             }
+        } else if (gameType == 2) {
+            choice = 0;
+        } else if (gameType == 3) {
+            int bound = moves.size() - 1;
+            if (bound < 1) {
+                bound = 1;
+            }
+            choice = new Random().nextInt(bound);
         }
-         */
+
         if (history.peek().isWon()) {
-            System.out.println("IT WON!");
+            System.out.println("YOU WON!");
             System.exit(0);
         } else if (moves.size() < 1) {
-            System.out.println("IT LOST.");
+            System.out.println("YOU LOST.");
             System.exit(0);
         }
-        int choice = 0;
+
         controller.performMove(moves.get(choice), new CompletionCallBack() {
             @Override
             public void OnSuccess(String status) {
