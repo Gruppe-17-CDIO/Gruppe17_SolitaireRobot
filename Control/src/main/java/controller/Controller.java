30e8e15f42d0500d0707662c2baafaf561da3c37
ErlendTyrmi <erlend@tyrmi.com>
2020-06-19 17:38:38 +0200

    undo method tested, test with CV necessary

--- Control/src/main/java/controller/Controller.java
+++ Control/src/main/java/controller/Controller.java
@@ -11,6 +11,7 @@ import logic.Logic;
 import stateBuilding.TopCardsSimulator;
 
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author Erlend
@@ -20,7 +21,7 @@ import java.util.List;
 public class Controller implements I_Controller {
     private I_Logic logic;
     private StateManager stateManager;
-    private I_ComputerVisionController CV_Controller = new Convertion();
+    private final I_ComputerVisionController CV_Controller = new Convertion();
     private boolean testmode = false;
     private Move currentMove;
     private TopCardsSimulator topCardsSimulator;
@@ -70,6 +71,9 @@ public class Controller implements I_Controller {
             startNewGame(img, callBack);
         } else {
             try {
+                // Get move from state before calculating new
+                currentMove = stateManager.getBestMove();
+
                 TopCards topCards;
                 SolitaireState state;
                 if (!testmode) {
@@ -86,8 +90,6 @@ public class Controller implements I_Controller {
                 stateManager.saveState(state);
                 stateManager.addMovesToState(moves);
 
-                // Reset move
-                currentMove = stateManager.getBestMove();
                 stateManager.updateGameProcess(moves);
                 callBack.OnSuccess(currentMove, stateManager.getHistory().peek(), state.getGameProgress());
             } catch (Exception e) {
@@ -101,10 +103,15 @@ public class Controller implements I_Controller {
         try {
             stateManager.undo();
             callBack.OnSuccess("UNDO registered. Last move and state logged, but deleted from current history. " +
-                    "\nPerform move again to continue");
+                    "\nPerform calculation again to continue");
         } catch (Exception e) {
             callBack.OnError(e);
         }
+    }
+
+    // This method is for testing, not part of interface
+    public Stack<SolitaireState> getHistory() throws Exception {
+        return stateManager.getHistory();
     }
 
     @Override
--- Control/src/main/java/controller/ManualTests/controllerUNDO_Test.java
+++ Control/src/main/java/controller/ManualTests/controllerUNDO_Test.java
@@ -3,6 +3,7 @@ package controller.ManualTests;
 import controller.CompletionCallBack;
 import controller.Controller;
 import controller.NextMoveCallBack;
+import dataObjects.Card;
 import dataObjects.GlobalEnums;
 import dataObjects.Move;
 import dataObjects.SolitaireState;
@@ -10,25 +11,20 @@ import javafx.scene.image.Image;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Stack;
 
-/**
- * Manual test of controller.
- *
- * @author erlend
- */
-public class ControllerTestTUI {
+public class controllerUNDO_Test {
     private Controller controller;
-    private Timestamp startTime;
+    private SolitaireState beforeLastState;
+    private int turnCount = 0;
+    private boolean errorFound = false;
 
     public static void main(String[] args) {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("WELCOME TO SUPER-TUI.");
-        System.out.println("The tui where you test your controller.");
+        System.out.println("Test of the UNDO-method.");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        new ControllerTestTUI().runGame();
+        new controllerUNDO_Test().runGame();
     }
 
 
@@ -55,8 +51,6 @@ public class ControllerTestTUI {
     }
 
     private void setupGame() {
-
-        startTime = new Timestamp(System.currentTimeMillis());
 
         controller.startNewGame(new Image(new InputStream() { // Dummy InputStream
             @Override
@@ -88,24 +82,38 @@ public class ControllerTestTUI {
     }
 
     private void playTurn(Move move, SolitaireState state) {
-        int choice = 0;
-
-        if (state.getGameProgress() == GlobalEnums.GameProgress.WON) {
-            System.out.println("YOU WON!");
-            System.out.println("It only took " +
-                    ((new Timestamp(System.currentTimeMillis()).getTime() - startTime.getTime())) + " millis!");
-            System.exit(0);
-        } else if (state.getGameProgress() == GlobalEnums.GameProgress.LOST) {
-            System.out.println("YOU LOST.");
-            System.out.println("It took " +
-                    ((new Timestamp(System.currentTimeMillis()).getTime() - startTime.getTime())) + " millis!");
-            System.exit(0);
+        if (turnCount == 1) {
+            beforeLastState = state;
         }
-        seeResults();
+        if (turnCount > 1) {
+            beforeLastState = state;
+            controller.undo(new CompletionCallBack() {
+                @Override
+                public void OnSuccess(String status) {
+                    System.out.println("\n**************" +
+                            "\nUNDO LAST MOVE" +
+                            "\n**************\n");
+
+                    validate(state);
+                }
+
+                @Override
+                public void OnFailure(String message) {
+                    System.out.println(message);
+                }
+
+                @Override
+                public void OnError(Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        performMove();
 
     }
 
-    private void seeResults() {
+    private void performMove() {
         controller.getNextMove(new Image(new InputStream() { // Dummy InputStream
             @Override
             public int read() throws IOException {
@@ -119,6 +127,8 @@ public class ControllerTestTUI {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                turnCount++;
+                System.out.println(turnCount);
                 playTurn(move, state);
             }
 
@@ -133,5 +143,51 @@ public class ControllerTestTUI {
             }
         });
     }
+
+
+    private void validate(SolitaireState state) {
+        List<Move> moves = state.getSuggestedMoves();
+
+        for (int i = 0; i < moves.size() - 1; i++) {
+            if (!(moves.get(i).toString().equals(beforeLastState.getSuggestedMoves().get(i).toString()))) {
+                errorFound = true;
+                System.out.println(
+                        "Error!: \n" +
+                                moves.get(i).toString() +
+                                "\ndoes not match \n" +
+                                beforeLastState.getSuggestedMoves().get(i).toString() + ".");
+            }
+        }
+
+        List<List<Card>> piles = state.getPiles();
+        List<List<Card>> oldPiles = beforeLastState.getPiles();
+        for (int pileIndex = 0; pileIndex < piles.size() - 1; pileIndex++) {
+            List<Card> cards = piles.get(pileIndex);
+            List<Card> oldCards = oldPiles.get(pileIndex);
+            for (int cardIndex = 0; cardIndex < cards.size() - 1; cardIndex++) {
+                if (!(cards.get(cardIndex).toString().equals(oldCards.get(cardIndex).toString()))) {
+                    errorFound = true;
+                    System.out.println(
+                            "Error!: \n" +
+                                    cards.get(cardIndex).toString() +
+                                    "\ndoes not match \n" +
+                                    oldCards.get(cardIndex).toString() + ".");
+                }
+            }
+        }
+
+
+        if (!errorFound) {
+            System.out.println("Test passed: The moves and piles match.");
+
+            System.out.println("Comparing ID's:");
+            System.out.println(state.time);
+            System.out.println(beforeLastState.time);
+        }
+
+        System.exit(0);
+    }
 }
+
+
 
